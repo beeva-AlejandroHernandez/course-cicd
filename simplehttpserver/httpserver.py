00382a6241cd8a5de786e7d27fcf798ffa70ecc2
Alejandro Hernandez <alejandro.hernandez@beeva.com>
2016-12-22 11:01:07 +0100

    cambiado el puerto

--- simplehttpserver/httpserver.py
+++ simplehttpserver/httpserver.py
@@ -5,7 +5,7 @@ PORT = 8001
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = socketserver.TCPServer(("8011", PORT), Handler)
 
 print("serving at port", PORT)
 httpd.serve_forever()
